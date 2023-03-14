bf68087178b82eb8e74330c9bbac33ceb4cc41d5
adriendavin <adriendavin@hotmail.fr>
2023-03-14 19:19:24 +0100

    card JSX ok

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,23 @@
+// import imageUrl from 'https://unsplash.com/photos/WLxQvbMyfas';
 
 export default function Card (props) {
   console.log(props)
 
   return (
     <div className='card'>
-    <h3>hi from card.js
+    {/* <h3>hi from card.js
       make 4 cards
-    </h3>
-  {/* <img src={props.img} alt="" className="card--image"/> */}
+    </h3> */}
+  <img src={props.item.imageUrl} alt="" className="card--image"/>
     {/* <img src={`../${props.item.imageUrl}`} alt="" className="card--image"/> */}
-    {/* <img src= {swimLogo} alt="" className="card--image"/> */}
-    <div className='card--stats'>
-       <span><i class="fa-solid fa-star"></i>{props.item.stats.rating}</span>
-       <span className='grey'>-({props.item.stats.reviewCount}) </span>
-       <span className='grey'>-{props.item.location}</span>
-    </div>
-    <p > {props.item.title}</p>
-    <p > {props.description}</p>
-    <p > <span className='bold'>From {props.item.price} $USD</span> / person</p>
+
+    <p> <i class="fa-solid fa-location-dot"></i> {props.item.location}</p>
+    <a href={props.item.googleMapsUrl}>view on Google Maps</a>
+    <p> {props.item.title}</p>
+    <p> {props.item.startDate}</p>
+    <p> {props.item.endDate}</p>
+    <p> {props.item.description}</p>
+
   </div>
   )
 
--- src/data.js
+++ src/data.js
@@ -2,10 +2,10 @@ export default [
   {
     title: "Mount Fuji",
     location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    googleMapsUrl: "https://www.google.com/maps/place/Mont+Fuji/@35.3606247,138.7186086,15z/data=!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA",
     startDate: "12 Jan, 2022",
     endDate: "24 Jan, 2022",
     description:"Mount Fuji is the tallest mountain in Japan, standing at 3776 meters. Mount Fuji is the single moste popular site in Japan, for both Japanese and foreign tourists.",
-    imageUrl:"https://unsplash.com/photos/WLxQvbMyfas"
+    imageUrl:"https://images.unsplash.com/photo-1589308078059-be1415eab4c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
   }
 ]
